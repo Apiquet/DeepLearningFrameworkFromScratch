fb7cad340a21c3c3b3614759625f7017e8e44846
Anthony <anthony.piquet2@gmail.com>
2020-04-12 18:15:06 +0200

    Upload homemade framework from github.com/Apiquet/gradient_descent

---
+++ homemade_framework/test_training.py
@@ -0,0 +1,36 @@
+from framework import *
+
+train_features, train_target = generate_disc_set(1000)
+test_features, test_target = generate_disc_set(1000)
+plot_dataset(train_features, train_target)
+
+nb_epochs = 400
+batch_size = 10
+
+# Build the model
+Model = Sequential([Linear(2,128), LeakyReLU(), Linear(128,2), LeakyReLU()], LossMSE())
+# Set the learning rate
+Model.set_Lr(0.01)
+
+# Print model's parameters
+Model.print(print_color=False)
+
+# Convert train_target to one hot encoding
+train_target_one_hot = convert_to_one_hot_labels(train_features, train_target)
+
+print_current_results(0, Model, train_features, train_target, test_features, test_target, 0, prefix = "Before training: ")
+test_results = []
+for epochs in range(0, nb_epochs):
+    loss_sum = 0
+    test_results.append(get_inferences(Model, test_features))
+    for b in range(train_features.shape[0] // batch_size):
+        output = Model.forward(train_features[list(range(b * batch_size, (b+1) * batch_size))])
+        loss = Model.backward(train_target_one_hot[list(range(b * batch_size, (b+1) * batch_size))], output)
+        loss_sum = loss_sum + loss.item()
+    if epochs % 30 == 0:
+        print_current_results(epochs + 1, Model, train_features, train_target, test_features, test_target, loss_sum)
+    
+        
+print_current_results(epochs, Model, train_features, train_target, test_features, test_target, loss_sum, prefix = "After training: ")
+
+    
