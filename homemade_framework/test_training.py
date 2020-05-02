77e01f4b925155684a0597939c1d2e09c986a175
Anthony <anthony.piquet2@gmail.com>
2020-05-02 17:58:27 +0200

    Add comments and add a variable for lr

--- homemade_framework/test_training.py
+++ homemade_framework/test_training.py
@@ -1,12 +1,13 @@
 from framework import *
 
+# generate train and set set
 train_features, train_target = generate_disc_set(1000)
 test_features, test_target = generate_disc_set(1000)
-
-nb_epochs = 400
+# set training en model parameters
+nb_epochs = 200
 batch_size = 10
 hidden_size = 50
-
+learning_rate = 0.003
 # Build the model
 Model = Sequential([Linear(2, hidden_size),
                     LeakyReLU(),
@@ -15,10 +16,9 @@ Model = Sequential([Linear(2, hidden_size),
                     Linear(hidden_size, 2),
                     Softmax()], LossMSE())
 # Set the learning rate
-Model.set_Lr(0.003)
-
+Model.set_Lr(learning_rate)
 # Print model's parameters
 Model.print(print_color=False)
-
+# start training
 train_homemade_model(Model, nb_epochs, train_features, train_target,
                      test_features, test_target, batch_size)
