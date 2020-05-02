b0b58457ff47b93d9a47481cb630d24e5ad00b9a
Anthony <anthony.piquet2@gmail.com>
2020-05-02 13:56:56 +0200

    Call framework train function from test training script

--- homemade_framework/test_training.py
+++ homemade_framework/test_training.py
@@ -20,22 +20,5 @@ Model.set_Lr(0.003)
 # Print model's parameters
 Model.print(print_color=False)
 
-# Convert train_target to one hot encoding
-train_target_one_hot = convert_to_one_hot_labels(train_features, train_target)
-
-print_current_results(0, Model, train_features, train_target, test_features, test_target, 0, prefix = "Before training: ")
-test_results = []
-for epochs in range(0, nb_epochs):
-    loss_sum = 0
-    test_results.append(get_inferences(Model, test_features))
-    for b in range(train_features.shape[0] // batch_size):
-        output = Model.forward(train_features[list(range(b * batch_size, (b+1) * batch_size))])
-        loss = Model.backward(train_target_one_hot[list(range(b * batch_size, (b+1) * batch_size))], output)
-        loss_sum = loss_sum + loss.item()
-    if epochs % 30 == 0:
-        print_current_results(epochs + 1, Model, train_features, train_target, test_features, test_target, loss_sum)
-    
-        
-print_current_results(epochs, Model, train_features, train_target, test_features, test_target, loss_sum, prefix = "After training: ")
-
-    
+train_homemade_model(Model, nb_epochs, train_features, train_target,
+                     test_features, test_target, batch_size)
