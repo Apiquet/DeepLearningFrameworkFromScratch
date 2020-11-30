0ef7bbf12f267dc6436249150ad5c15d7ae6f21f
Anthony <anthony.piquet2@gmail.com>
2020-11-30 22:20:24 +0100

    [DEV][DB] Train test split

--- DB/db_manager.py
+++ DB/db_manager.py
@@ -17,7 +17,7 @@ class DBManager():
     def __init__(self):
         super(DBManager, self).__init__()
 
-    def load_data(db_path):
+    def load_data(db_path, train_ratio=0.7):
         """
         Method to load the database
         N: number of images for a class
@@ -30,15 +30,27 @@ class DBManager():
             - (numpy array) classes (N)
         """
         imgs_path = glob(db_path + "/*")
-        imgs = []
-        labels = []
-        random_idx = np.arange(len(imgs_path))
+        number_of_imgs = len(imgs_path)
+        train_number = int(number_of_imgs*train_ratio)
+        random_idx = np.arange(number_of_imgs)
         np.random.shuffle(random_idx)
 
-        for idx in random_idx:
+        train_imgs = []
+        train_labels = []
+        test_imgs = []
+        test_labels = []
+
+        for i, idx in enumerate(random_idx):
             img_path = imgs_path[idx]
             image = Image.open(img_path)
-            imgs.append(np.asarray(image).reshape([1, 28, 28]))
             img_basename = os.path.splitext(os.path.basename(img_path))[0]
-            labels.append(int(img_basename.split('_')[2]))
-        return imgs, labels
+
+            if i <= train_number:
+                train_imgs.append(np.asarray(image).reshape([1, 28, 28]))
+                train_labels.append(int(img_basename.split('_')[2]))
+            else:
+                test_imgs.append(np.asarray(image).reshape([1, 28, 28]))
+                test_labels.append(int(img_basename.split('_')[2]))
+
+        return np.array(train_imgs), np.array(train_labels),\
+            np.array(test_imgs), np.array(test_labels)
