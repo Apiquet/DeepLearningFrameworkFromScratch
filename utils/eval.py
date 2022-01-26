c08eccf91a47c62934fbf44a08b1136c56c7ed3e
Apiquet <anthony.piquet2@gmail.com>
2022-01-26 14:54:54 -0600

    [DEV][EVAL] Format

--- utils/eval.py
+++ utils/eval.py
@@ -48,7 +48,7 @@ def concat4Gif(path_1, path_2, path_3, path_4, out_path, fps=22, line_width=2):
     for frame in gif_4:
         gif_4_imgs.append(frame)
 
-    total_size = (gif_1_imgs[0].shape[0]*2, gif_1_imgs[0].shape[1]*2, 3)
+    total_size = (gif_1_imgs[0].shape[0] * 2, gif_1_imgs[0].shape[1] * 2, 3)
     white_image = 255 * np.ones(total_size, np.uint8)
     white_pil = Image.fromarray(white_image)
 
@@ -59,27 +59,24 @@ def concat4Gif(path_1, path_2, path_3, path_4, out_path, fps=22, line_width=2):
         pil_img_1 = Image.fromarray(np_img_1)
         draw = ImageDraw.Draw(pil_img_1)
         min_point = (-10, 0)
-        end_point = (pil_img_1.size[0], pil_img_1.size[1]+10)
-        draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                       width=line_width)
-        white_pil.paste(pil_img_1, (0,  pil_img_1.size[1]))
+        end_point = (pil_img_1.size[0], pil_img_1.size[1] + 10)
+        draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
+        white_pil.paste(pil_img_1, (0, pil_img_1.size[1]))
 
         np_img_2 = np.asarray(gif_2_imgs[idx])
         pil_img_2 = Image.fromarray(np_img_2)
         draw = ImageDraw.Draw(pil_img_2)
         min_point = (0, 0)
-        end_point = (pil_img_2.size[0]+10, pil_img_2.size[1]+10)
-        draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                       width=line_width)
+        end_point = (pil_img_2.size[0] + 10, pil_img_2.size[1] + 10)
+        draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
         white_pil.paste(pil_img_2, (pil_img_2.size[0], pil_img_2.size[1]))
 
         np_img_3 = np.asarray(gif_3_imgs[idx])
         pil_img_3 = Image.fromarray(np_img_3)
         draw = ImageDraw.Draw(pil_img_3)
         min_point = (0, -10)
-        end_point = (pil_img_3.size[0]+10, pil_img_3.size[1])
-        draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                       width=line_width)
+        end_point = (pil_img_3.size[0] + 10, pil_img_3.size[1])
+        draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
         white_pil.paste(pil_img_3, (pil_img_3.size[0], 0))
 
         np_img_4 = np.asarray(gif_4_imgs[idx])
@@ -87,15 +84,12 @@ def concat4Gif(path_1, path_2, path_3, path_4, out_path, fps=22, line_width=2):
         draw = ImageDraw.Draw(pil_img_4)
         min_point = (-10, -10)
         end_point = (pil_img_4.size[0], pil_img_4.size[1])
-        draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                       width=line_width)
+        draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
         white_pil.paste(pil_img_4, (0, 0))
 
         images.append(white_pil.copy())
 
-    images[0].save(out_path, format='GIF',
-                   append_images=images[1:],
-                   save_all=True, loop=0)
+    images[0].save(out_path, format='GIF', append_images=images[1:], save_all=True, loop=0)
     gif = imageio.mimread(out_path)
     imageio.mimsave(out_path, gif, fps=fps)
 
@@ -120,36 +114,32 @@ def concat4Images(path_1, path_2, path_3, path_4, out_path, line_width=2):
     pil_img_3 = Image.open(path_3)
     pil_img_4 = Image.open(path_4)
 
-    total_size = (pil_img_1.size[0]*2, pil_img_1.size[1]*2, 3)
+    total_size = (pil_img_1.size[0] * 2, pil_img_1.size[1] * 2, 3)
     white_image = 255 * np.ones(total_size, np.uint8)
     white_pil = Image.fromarray(white_image)
 
     draw = ImageDraw.Draw(pil_img_1)
     min_point = (-10, 0)
-    end_point = (pil_img_1.size[0], pil_img_1.size[1]+10)
-    draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                   width=line_width)
-    white_pil.paste(pil_img_1, (0,  pil_img_1.size[1]))
+    end_point = (pil_img_1.size[0], pil_img_1.size[1] + 10)
+    draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
+    white_pil.paste(pil_img_1, (0, pil_img_1.size[1]))
 
     draw = ImageDraw.Draw(pil_img_2)
     min_point = (0, 0)
-    end_point = (pil_img_2.size[0]+10, pil_img_2.size[1]+10)
-    draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                   width=line_width)
+    end_point = (pil_img_2.size[0] + 10, pil_img_2.size[1] + 10)
+    draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
     white_pil.paste(pil_img_2, (pil_img_2.size[0], pil_img_2.size[1]))
 
     draw = ImageDraw.Draw(pil_img_3)
     min_point = (0, -10)
-    end_point = (pil_img_3.size[0]+10, pil_img_3.size[1])
-    draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                   width=line_width)
+    end_point = (pil_img_3.size[0] + 10, pil_img_3.size[1])
+    draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
     white_pil.paste(pil_img_3, (pil_img_3.size[0], 0))
 
     draw = ImageDraw.Draw(pil_img_4)
     min_point = (-10, -10)
     end_point = (pil_img_4.size[0], pil_img_4.size[1])
-    draw.rectangle((min_point, end_point), outline=(255, 255, 255),
-                   width=line_width)
+    draw.rectangle((min_point, end_point), outline=(255, 255, 255), width=line_width)
     white_pil.paste(pil_img_4, (0, 0))
 
     white_pil.save(out_path)
